b5eba746cf8a72a66a64f6acf0582641db275a5f
marianaarodriguess18 <marianasalome02@hotmail.com>
2024-11-19 15:08:14 +0000

    alterações

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ let fontSizeTitle = 24;
 
 const textContent = {
     en: {
-        title: "LIVING WITH FATIGUE AND PAIN",
+        title: "Living with fatigue and pain",
         text: "Lea was on track to become the editor of the magazine she worked for when she started having numbness in her hands and feeling completely fatigued by the middle of the afternoon. She tried medications and exercise and getting enough sleep, but finally she had to make change in her life. She found a job where she could work from home, on her own schedule. When she has good days, it's like nothing is wrong. But on bad days, she measures every action so she can make it through the day. Sometime that important editorial meeting is all she can manage. She had to adjust her computer: a new keyboard and trackBall make it easier to type, and a good chair helps her avoid tender muscles. The biggest change was learning to write and edit using speech recognition software, Dragon Naturally Speaking. She's lucky: the company understands that it's a real disability. With an invisible disability like fibromyalgia, some people just don't get it.",
         increaseFont: "Increase Font",
         decreaseFont: "Decrease Font",
@@ -26,7 +26,7 @@ const textContent = {
         speechRecognitionBtn: "Speech recognition"  
     },
     pt: {
-        title: "VIVER COM FADIGA E DOR",
+        title: "Viver com fadiga e dor",
         text: "Lea estava a caminho de se tornar editora da revista para a qual trabalhava quando começou a ter dormência nas mãos e a sentir-se completamente cansada a meio da tarde. Tentou tomar medicamentos, fazer exercício físico e dormir o suficiente, mas finalmente teve de fazer uma mudança na sua vida. Encontrou um emprego onde podia trabalhar a partir de casa, de acordo com o seu próprio horário. Quando tem dias bons, é como se nada estivesse errado. Mas nos dias maus, mede todas as ações para conseguir passar o dia. Por vezes, aquela importante reunião editorial é tudo o que consegue fazer. Teve de adaptar o seu computador: um novo teclado e uma TrackBall facilitam a escrita e uma boa cadeira ajuda-a a evitar músculos doridos. A maior mudança foi aprender a escrever e editar utilizando o software de reconhecimento de voz, Dragon Naturally Speaking. Ela tem sorte: a empresa compreende que se trata de uma deficiência real. Com uma deficiência invisível como a fibromialgia, algumas pessoas simplesmente não percebem.",
         increaseFont: "Aumentar Fonte",
         decreaseFont: "Diminuir Fonte",
